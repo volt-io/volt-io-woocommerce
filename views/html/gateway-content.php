1681bd360e46c3e7fd2b82f7e80aaead5f945c41
studiodev88 <sebastian.drewniak@gmail.com>
2023-05-10 13:58:10 +0200

    init

---
+++ views/html/gateway-content.php
@@ -0,0 +1,18 @@
+
+<div id="volt-payment-component" data-voltio-mode="<?php echo $this->mode ?>"
+     data-widget_voltio_font_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_font_color']) ?>"
+     data-widget_voltio_background_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_background_color']) ?>"
+     data-widget_voltio_input_text_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_input_text_color']) ?>"
+     data-widget_voltio_input_border_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_input_border_color']) ?>"
+     data-widget_voltio_input_background_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_input_background_color']) ?>"
+     data-widget_voltio_pay_button_text_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_pay_button_text_color']) ?>"
+     data-widget_voltio_pay_button_border_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_pay_button_border_color']) ?>"
+     data-widget_voltio_pay_button_background_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_pay_button_background_color']) ?>"
+     data-widget_voltio_link_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_link_color']) ?>"
+     data-widget_voltio_icon_color="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_icon_color']) ?>"
+     data-widget_voltio_input_border_radius="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_input_border_radius']) ?>"
+     data-widget_voltio_pay_button_border_radius="<?php echo $this->helper->get_voltio_option(['woocommerce_voltio_settings', 'widget_voltio_pay_button_border_radius']) ?>"
+></div>
+<input type="hidden" name="voltio-selected-bank" value="0" />
+<input type="hidden" name="voltio-hash" value="0" />
+<div id="volt-payment-terms"></div>
---
+++ views/html/modal.php
@@ -0,0 +1,35 @@
+<div class="volt-modal" data-volt-icon="<?php echo '/wp-content/plugins/' . VOLTIO_PLUGIN_DIR . '/views/img/volt-info.svg' ?>">
+    <div class="volt-content-modal">
+        <div class="volt-left">
+            <p class="volt-title"><?php _e('How Volt works') ?></p>
+            <p class="volt-subtitle"><?php _e('Check out in three easy steps:') ?></p>
+            <div class="volt-steps">
+                <div class="volt-step">
+                    <div class="volt-number">1</div>
+                    <div class="volt-content">
+                        <p><?php _e('Select your bank (99% of banks supported)') ?></p>
+                        <p><?php _e('Pay from your bank. No card needed.') ?></p>
+                    </div>
+                </div>
+                <div class="volt-step">
+                    <div class="volt-number">2</div>
+                    <div class="volt-content">
+                        <p><?php _e('Log into your account') ?></p>
+                        <p><?php _e('Your bank details are never shared.') ?></p>
+                    </div>
+                </div>
+                <div class="volt-step">
+                    <div class="volt-number">3</div>
+                    <div class="volt-content">
+                        <p><?php _e('Approve the payment') ?></p>
+                        <p><?php _e('That’s it. Faster and more secure.') ?></p>
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="volt-close"><?php _e('Continue') ?></a>
+        </div>
+        <div class="volt-right">
+            <img src="<?php echo '/wp-content/plugins/' . VOLTIO_PLUGIN_DIR . '/views/img/volt-modal.png' ?>" />
+        </div>
+    </div>
+</div>
